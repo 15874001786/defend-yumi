1451fe070496f7ea1245ad664b343e3ff6972009
刘咸安 <xianan.liu@dibcn.com>
2026-05-27 18:09:47 +0800

    chore:env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH ?? '/defend-yumi/',
+export default defineConfig(({ command }) => ({
+  base: process.env.VITE_BASE_PATH ?? (command === 'build' ? '/defend-yumi/dist/' : '/'),
   server: {
     port: 5173,
   },
   preview: {
     port: 4173,
   },
-});
+}));
